0d44fb3fd3036b84fc87cdc9ae3d504d5312d2c5
ssamarin <semenssamarin@mail.ru>
2022-11-16 17:41:46 +1000

    add header slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 $(function() {
     $('.header__slider').slick({
         infinite: true,
-        fade: true
+        fade: true,
+        prevArrow: '<img class="header__slider_arrows header__slider_arrows-left" src="icons/arrow_left.svg" alt="arrow">',
+        nextArrow: '<img class="header__slider_arrows header__slider_arrows-right" src="icons/arrow_right.svg" alt="arrow">'
     });
 });
