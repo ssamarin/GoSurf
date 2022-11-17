11d25d3afb42df21d5505d513763acc8fae6e8cb
ssamarin <semenssamarin@mail.ru>
2022-11-17 18:07:22 +1000

    add slider dots

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,15 @@ $(function() {
         infinite: true,
         fade: true,
         prevArrow: '<img class="header__slider_arrows header__slider_arrows-left" src="icons/arrow_left.svg" alt="arrow">',
-        nextArrow: '<img class="header__slider_arrows header__slider_arrows-right" src="icons/arrow_right.svg" alt="arrow">'
+        nextArrow: '<img class="header__slider_arrows header__slider_arrows-right" src="icons/arrow_right.svg" alt="arrow">',
+        asNavFor: '.header__dots ',
+    });
+    $('.header__dots').slick({
+        slidesToShow: 4,
+        infinite: true,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
     });
 });
+
+
