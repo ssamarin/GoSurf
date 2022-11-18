e9ab1f60dd1b2df0867aaa6ed84dd59daa929b1b
ssamarin <semenssamarin@mail.ru>
2022-11-18 18:34:28 +1000

    add aside

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,8 +2,8 @@ $(function() {
     $('.header__slider').slick({
         infinite: true,
         fade: true,
-        prevArrow: '<img class="header__slider_arrows header__slider_arrows-left" src="icons/arrow_left.svg" alt="arrow">',
-        nextArrow: '<img class="header__slider_arrows header__slider_arrows-right" src="icons/arrow_right.svg" alt="arrow">',
+        prevArrow: '<img class="header__slider_arrows header__slider_arrows-left" src="icons/arrows/arrow_left.svg" alt="arrow">',
+        nextArrow: '<img class="header__slider_arrows header__slider_arrows-right" src="icons/arrows/arrow_right.svg" alt="arrow">',
         asNavFor: '.header__dots ',
     });
     $('.header__dots').slick({
