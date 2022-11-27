453366e4034af7e93bac7176aa9a45bd4fa4b68c
ssamarin <semenssamarin@mail.ru>
2022-11-27 16:59:11 +1000

    add surf slider markup

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,11 +6,18 @@ $(function() {
         nextArrow: '<img class="header__slider_arrows header__slider_arrows-right" src="icons/arrows/arrow_right.svg" alt="arrow">',
         asNavFor: '.header__dots ',
     });
+
     $('.header__dots').slick({
         slidesToShow: 4,
         infinite: true,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
+    });
+    $('.surf__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="header__slider_arrows header__slider_arrows-left" src="icons/arrows/arrow_left.svg" alt="arrow">',
+        nextArrow: '<img class="header__slider_arrows header__slider_arrows-right" src="icons/arrows/arrow_right.svg" alt="arrow">',
     });
 });
 
