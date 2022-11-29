86cf25985ed5ce99d88250a72eb59516374c35cd
ssamarin <semenssamarin@mail.ru>
2022-11-29 18:31:56 +1000

    remove surf slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -13,12 +13,6 @@ $(function() {
         slidesToScroll: 4,
         asNavFor: '.header__slider',
     });
-    $('.surf__slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<img class="header__slider_arrows header__slider_arrows-left" src="icons/arrows/arrow_left.svg" alt="arrow">',
-        nextArrow: '<img class="header__slider_arrows header__slider_arrows-right" src="icons/arrows/arrow_right.svg" alt="arrow">',
-    });
 });
 
 
